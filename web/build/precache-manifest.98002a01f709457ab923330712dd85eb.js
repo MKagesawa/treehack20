443dc76014454c8ec31e63082a079c08e5ca33e5
Masaki <mk5986@nyu.edu>
2020-02-15 00:18:59 -0800

    Mod: dir for frontend backend

---
+++ web/build/precache-manifest.98002a01f709457ab923330712dd85eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0ecb4a045e3281fca1f3a9e904eeb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58d30a8955581aaa268f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4712eca1dd83a001d550",
+    "url": "/static/js/2.7c394083.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.7c394083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58d30a8955581aaa268f",
+    "url": "/static/js/main.0e602d2e.chunk.js"
+  },
+  {
+    "revision": "bb218cd3a880952c3a44",
+    "url": "/static/js/runtime-main.c44d3ef0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
