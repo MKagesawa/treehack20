77bbebb6996d9cbd88685cf5f5d6477e2eadc114
Vincent Teng <vincentt117@gmail.com>
2020-02-15 06:30:22 -0800

    Added RequestMap component

---
+++ web/src/components/RequestMap.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+class RequestMap extends React.Component {
+    render() {
+      return <h1>Hello, {this.props.name}</h1>;
+    }
+  }
+
+export default RequestMap;
