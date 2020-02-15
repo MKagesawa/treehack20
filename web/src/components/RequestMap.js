662fafd1aad300d50027a9970e1ca6e66b6cee91
Vincent Teng <vincentt117@gmail.com>
2020-02-15 07:03:33 -0800

    Added gmaps react component

--- web/src/components/RequestMap.js
+++ web/src/components/RequestMap.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import logo from '../logo.svg';
 import '../App.css';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const API_KEY = process.env.GOOGLE_MAPS_API_KEY;
 
 class RequestMap extends React.Component {
     render() {
-      return <h1>Hello, {this.props.name}</h1>;
-    }
+      return (
+        <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176}}
+        />
+    );
   }
+}
 
-export default RequestMap;
+export default RequestMap({
+  apiKey: API_KEY 
+})(MapContainer);
